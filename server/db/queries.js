9bc176da7fef5f5bda77a1bd90193fc261ea6703
AliNisarAhmed <ali.nisar87@gmail.com>
2019-02-07 22:41:34 +0500

    final

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,5 +1,10 @@
 const knex = require('./knex');
 
+async function findProjectId (projectname) {
+  const project = await knex('projects').select('id').where({projectname});
+  return project[0];
+}
+
 module.exports = {
   getAllProjects () {
     return knex('projects');
@@ -7,16 +12,22 @@ module.exports = {
   createProject(project) {
     return knex('projects').insert(project, "*");
   },
-  async findProjectId (projectname) {
-    const project = await knex('projects').select('id').where({projectname});
-    return project[0];
-  },
   async createIssue (projectname, issue) {
-    let projectId = await this.findProjectId(projectname).id;
-    let issueObj = Object.assign({}, {project_id: projectId}, issue);
+    let { id: projectId } = await findProjectId(projectname);
+    let issueObj = Object.assign({}, issue, {project_id: projectId});
     return knex('issues').insert(issueObj, "*");
   },
   getAllIssues (projectname, queryParams) {
     return knex('projects').select().rightJoin('issues', 'issues.project_id', '=', 'projects.id').where({projectname: projectname, ...queryParams});
+  },
+  async updateIssue (projectname, updates) {
+    const { id: projectId } = await findProjectId(projectname);
+    return knex('issues').where({project_id: projectId}).update({...updates, updated_on: (new Date()).toISOString() }, "*");
+  },
+  deleteIssue (id) {
+    return knex('issues').where({id}).del();
+  },
+  findIssueById (id) {
+    return knex('issues').select().where({id});
   }
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -27,15 +27,39 @@ router.post('/:project', async (req, res) => {
 })
 
 router.put('/:project', async (req, res) => {
-
+  // update any field of an issue in a particular project, by providing update object in the body
+  try {
+    let updates = req.body;
+    const keys = Object.keys(updates);
+    if (keys.length === 0) throw new Error('Must provide one field to update');
+    if (keys.includes('created_on') || keys.includes('id') || keys.includes('project_id')) throw new Error('Update request denied');
+    let projectname = req.params.project.toLowerCase();
+    let updatedIssue = await queries.updateIssue(projectname, updates);
+    res.status(200).json(updatedIssue);
+  } catch (error) {
+    res.status(500).json({error: error.message});    
+  }
 });
 
 router.delete('/:project', async (req, res) => {
-
+  // delete an issue from the db
+  try {
+    let issueId = req.body.id
+    const deletedIssue = await queries.deleteIssue(issueId);
+    if (deletedIssue === 1) {
+      res.status(200).json({success: `deleted Issue ${issueId}`})
+    } else {
+      res.status(404).json({failed: `could not delete ${issueId}`})
+    }
+  } catch (error) {
+    res.status(500).json({error: error.message});    
+  }
 });
 
 router.get('/:project/:issue_id', async (req, res) => {
-
+  const issueId = req.params.issue_id;
+  const issue = await queries.findIssueById(issueId);
+  res.status(200).json(issue[0]);
 })
 
 module.exports = router;
